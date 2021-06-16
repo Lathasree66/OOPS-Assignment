10c47823cd2c9d59df88c8a50bd6432e2adc0f85
Lathasree66 <85178535+Lathasree66@users.noreply.github.com>
2021-06-16 21:27:57 +0530

    Committing the Assignment1(Bank Account)

---
+++ FirstAssign/ClassLibrary1/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public abstract class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public List<string> AccountHolderName { get; set; }
+        public string AccountOpeningDate { get; set; }
+        public double BalanceAmount { get; set; }
+        public List<string> ListOfTransactions { get; set; }
+
+        public abstract void Debit(int debitamount);
+        public void Credit(int creditamount)
+        {
+            BalanceAmount = BalanceAmount + creditamount;
+        }
+
+        public double balanceAmount()
+        {
+            return BalanceAmount;
+        }
+    }
+    public class SavingsAccount : BankAccount
+    {
+        public override void Debit(int debitamount)
+        {
+            BalanceAmount = BalanceAmount - (debitamount+ (debitamount * 0.01));
+        }
+
+    }
+    public class CheckingAccount : BankAccount
+    {
+        public override void Debit(int debitamount)
+        {
+            BalanceAmount = BalanceAmount - (debitamount + (debitamount * 0.025));
+        }
+    }
+}
---
+++ FirstAssign/FirstAssign/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SavingsAccount Savobj = new SavingsAccount();
+            Savobj.AccountNumber = 12345;
+            Savobj.AccountHolderName = new List<string>() { "Steve","Hill"};
+            Savobj.AccountOpeningDate = "15-06-2021";
+            Savobj.BalanceAmount = 10000;
+            Savobj.Debit(500);
+            Console.WriteLine("Hey "+ Savobj.AccountHolderName[0] +" You withdrawn 500 and Bank Balance is: "+Savobj.balanceAmount());
+            Savobj.Credit(1000);
+            Console.WriteLine("Hey " + Savobj.AccountHolderName[0] + " You deposited 1000 and Remaining Bank Balance is: " + Savobj.balanceAmount());
+
+            CheckingAccount checkobj = new CheckingAccount();
+            checkobj.AccountNumber = 54321;
+            checkobj.AccountHolderName = new List<string>() {"Joseph"};
+            checkobj.AccountOpeningDate = "16-06-2021";
+            checkobj.BalanceAmount = 5000;
+            checkobj.Credit(6000);
+            Console.WriteLine("Hey " + checkobj.AccountHolderName[0] + " You deposited 6000 and Bank Balance is: " + checkobj.balanceAmount());
+            checkobj.Debit(1000);
+            Console.WriteLine("Hey " + checkobj.AccountHolderName[0] + " You withdrawn 1000 and Bank Balance is: " + checkobj.balanceAmount());
+
+            Savobj.Credit(5000);
+            Console.WriteLine("Hey " + Savobj.AccountHolderName[0] + " You deposited 5000 and Remaining Bank Balance is: " + Savobj.balanceAmount());
+
+            Console.ReadLine();
+
+        }
+    }
+}
