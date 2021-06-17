220577c1c1416b08b1c5621989f138f07836e128
Lathasree66 <85178535+Lathasree66@users.noreply.github.com>
2021-06-17 09:27:24 +0530

    Updating the assignment

--- FirstAssign/ClassLibrary1/BankAccount.cs
+++ FirstAssign/ClassLibrary1/BankAccount.cs
@@ -8,36 +8,66 @@ namespace ClassLibrary1
 {
     public abstract class BankAccount
     {
-        public int AccountNumber { get; set; }
-        public List<string> AccountHolderName { get; set; }
-        public string AccountOpeningDate { get; set; }
-        public double BalanceAmount { get; set; }
         public List<string> ListOfTransactions { get; set; }
 
-        public abstract void Debit(int debitamount);
+        public int AccountNumber;
+        public List<string> AccountHolderName;
+        public string AccountOpeningDate;
+        public double BalanceAmount;
+        public abstract void Debit(double debitamount);
         public void Credit(int creditamount)
         {
             BalanceAmount = BalanceAmount + creditamount;
+            balanceAmount("Deposited", creditamount);
         }
 
         public double balanceAmount()
         {
             return BalanceAmount;
         }
+
+        public void balanceAmount(string transactiontype, double amount)
+        {
+            if(transactiontype=="Withdrawn")
+            {
+                Console.WriteLine("Hey " + AccountHolderName[0] + " You Withdrawn "+ amount +" and Remaining Bank Balance is: " + BalanceAmount);
+            }
+            else
+            {
+                Console.WriteLine("Hey " + AccountHolderName[0] + " You Deposited " + amount + " and Remaining Bank Balance is: " + BalanceAmount);
+            }
+        }
     }
     public class SavingsAccount : BankAccount
     {
-        public override void Debit(int debitamount)
+        public SavingsAccount(int acnum, List<string> acholdername, string acopeningdate, double balamount)
+        {
+            AccountNumber = acnum;
+            AccountHolderName = acholdername;
+            AccountOpeningDate = acopeningdate;
+            BalanceAmount = balamount;
+        }
+        public override void Debit(double debitamount)
         {
             BalanceAmount = BalanceAmount - (debitamount+ (debitamount * 0.01));
+            balanceAmount("Withdrawn", debitamount);
         }
 
     }
     public class CheckingAccount : BankAccount
     {
-        public override void Debit(int debitamount)
+        public CheckingAccount(int acnum, List<string> acholdername, double balamount)
+        {
+            AccountNumber = acnum;
+            AccountHolderName = acholdername;
+            AccountOpeningDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); 
+            BalanceAmount = balamount;
+
+        }
+        public override void Debit(double debitamount)
         {
             BalanceAmount = BalanceAmount - (debitamount + (debitamount * 0.025));
+            balanceAmount("Withdrawn",debitamount);
         }
     }
 }
--- FirstAssign/FirstAssign/Program.cs
+++ FirstAssign/FirstAssign/Program.cs
@@ -11,29 +11,13 @@ namespace FirstAssign
     {
         static void Main(string[] args)
         {
-
-            SavingsAccount Savobj = new SavingsAccount();
-            Savobj.AccountNumber = 12345;
-            Savobj.AccountHolderName = new List<string>() { "Steve","Hill"};
-            Savobj.AccountOpeningDate = "15-06-2021";
-            Savobj.BalanceAmount = 10000;
-            Savobj.Debit(500);
-            Console.WriteLine("Hey "+ Savobj.AccountHolderName[0] +" You withdrawn 500 and Bank Balance is: "+Savobj.balanceAmount());
-            Savobj.Credit(1000);
-            Console.WriteLine("Hey " + Savobj.AccountHolderName[0] + " You deposited 1000 and Remaining Bank Balance is: " + Savobj.balanceAmount());
-
-            CheckingAccount checkobj = new CheckingAccount();
-            checkobj.AccountNumber = 54321;
-            checkobj.AccountHolderName = new List<string>() {"Joseph"};
-            checkobj.AccountOpeningDate = "16-06-2021";
-            checkobj.BalanceAmount = 5000;
+            SavingsAccount savobj = new SavingsAccount(1234, new List<string>() { "Steve", "Hill" }, "16 - 06 - 2021", 10000.00);
+            CheckingAccount checkobj = new CheckingAccount(1234, new List<string>() { "Joseph" }, 0.00);
+            savobj.Debit(500);
+            savobj.Credit(1000);
             checkobj.Credit(6000);
-            Console.WriteLine("Hey " + checkobj.AccountHolderName[0] + " You deposited 6000 and Bank Balance is: " + checkobj.balanceAmount());
             checkobj.Debit(1000);
-            Console.WriteLine("Hey " + checkobj.AccountHolderName[0] + " You withdrawn 1000 and Bank Balance is: " + checkobj.balanceAmount());
-
-            Savobj.Credit(5000);
-            Console.WriteLine("Hey " + Savobj.AccountHolderName[0] + " You deposited 5000 and Remaining Bank Balance is: " + Savobj.balanceAmount());
+            savobj.Credit(5000);
 
             Console.ReadLine();
 
