94ae6f5327db8388404ff9527793db769d254cc1
Lathasree66 <85178535+Lathasree66@users.noreply.github.com>
2021-06-17 22:10:41 +0530

    Updated the assignment and committing

--- FirstAssign/ClassLibrary1/BankAccount.cs
+++ FirstAssign/ClassLibrary1/BankAccount.cs
@@ -2,23 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
     public abstract class BankAccount
     {
-        public List<string> ListOfTransactions { get; set; }
-
+        
         public int AccountNumber;
         public List<string> AccountHolderName;
         public string AccountOpeningDate;
         public double BalanceAmount;
-        public abstract void Debit(double debitamount);
-        public void Credit(int creditamount)
+
+        public List<Transaction> ListOfTransactions = new List<Transaction>();
+        public class Transaction
         {
+            public int TransactionAc { get; set; }
+            public string TransactionHolderName { get; set; }
+            public DateTime TransactionDate { get; set; }
+            public TransactionType TransactionType { get; set; }
+            public string Description { get; set; }
+            public double TransactionAmount { get; set; }
+        }
+        public enum TransactionType
+        {
+            Deposit,
+            Withdrawal
+        }
+
+         public void ViewTransactions()
+        {
+
+            if (ListOfTransactions.Count <= 0)
+                Console.WriteLine("There is no transaction yet.");
+            else
+            {
+                if (ListOfTransactions.Count > 5)
+                {
+                    for(int index=0; index <= ListOfTransactions.Count;index++)
+                    {
+                        ListOfTransactions.RemoveAt(index);
+                        if(ListOfTransactions.Count == 5)
+                        {
+                            break;
+                        } 
+                    }
+                }
+                foreach (var tran in ListOfTransactions)
+                {
+                    Console.WriteLine("Account " + tran.TransactionAc + " of " + tran.TransactionHolderName + " has " + tran.TransactionType + " " + tran.TransactionAmount + " on " + tran.TransactionDate);
+                }
+            }
+        }
+        Transaction transaction = new Transaction();
+        public void InsertTransaction(Transaction transaction)
+        {
+            ListOfTransactions.Add(transaction);
+        }
+        
+        public abstract void Debit(double debitamount);
+        public void Credit(double creditamount)
+        {
+            if (creditamount > 1000.00)
+                throw new ArgumentException("Cannot transfer more than 1000.00");
             BalanceAmount = BalanceAmount + creditamount;
             balanceAmount("Deposited", creditamount);
+            var transaction = new Transaction()
+            {
+                TransactionDate = DateTime.Now,
+                TransactionType = TransactionType.Deposit,
+                TransactionAmount = creditamount,
+                TransactionHolderName = AccountHolderName[0],
+                TransactionAc = AccountNumber
+            };
+            InsertTransaction(transaction);
         }
 
         public double balanceAmount()
@@ -26,9 +84,9 @@ namespace ClassLibrary1
             return BalanceAmount;
         }
 
-        public void balanceAmount(string transactiontype, double amount)
+        public void balanceAmount(string transtype, double amount)
         {
-            if(transactiontype=="Withdrawn")
+            if(transtype=="Withdrawn")
             {
                 Console.WriteLine("Hey " + AccountHolderName[0] + " You Withdrawn "+ amount +" and Remaining Bank Balance is: " + BalanceAmount);
             }
@@ -42,32 +100,64 @@ namespace ClassLibrary1
     {
         public SavingsAccount(int acnum, List<string> acholdername, string acopeningdate, double balamount)
         {
-            AccountNumber = acnum;
-            AccountHolderName = acholdername;
-            AccountOpeningDate = acopeningdate;
-            BalanceAmount = balamount;
+                Regex rexObj = new Regex(@"^\d{9}");
+                if (rexObj.IsMatch(Convert.ToString(acnum)) == false)
+                    throw new ArgumentException("Account Number cannot be less than nine digits.Please Enter Valid Number:");
+                AccountNumber = acnum;
+                AccountHolderName = acholdername;
+                AccountOpeningDate = acopeningdate;
+                BalanceAmount = balamount;
         }
         public override void Debit(double debitamount)
         {
-            BalanceAmount = BalanceAmount - (debitamount+ (debitamount * 0.01));
-            balanceAmount("Withdrawn", debitamount);
+            if(debitamount>BalanceAmount)
+                throw new ArgumentException("Withdrawl amount cannot be greater than Account Balance");
+            if (debitamount > 1000.00)
+                throw new ArgumentException("Cannot transfer more than 1000.00");
+            BalanceAmount = BalanceAmount - (debitamount + (debitamount * 0.01));
+                balanceAmount("Withdrawn", debitamount);
+                var transaction = new Transaction()
+                {
+                    TransactionDate = DateTime.Now,
+                    TransactionType = TransactionType.Withdrawal,
+                    TransactionAmount = debitamount,
+                    TransactionHolderName = AccountHolderName[0],
+                    TransactionAc = AccountNumber
+                };
+                InsertTransaction(transaction);
         }
-
     }
-    public class CheckingAccount : BankAccount
+    public class CheckingAccount : BankAccount 
     {
         public CheckingAccount(int acnum, List<string> acholdername, double balamount)
         {
-            AccountNumber = acnum;
-            AccountHolderName = acholdername;
-            AccountOpeningDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); 
-            BalanceAmount = balamount;
-
+                Regex rexObj = new Regex(@"^\d{9}");
+                if (rexObj.IsMatch(Convert.ToString(acnum)) == false)
+                    throw new ArgumentException("Account Number cannot be less than nine digits.Please Enter Valid Number:");
+                AccountNumber = acnum;
+                AccountHolderName = acholdername;
+                AccountOpeningDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); 
+                BalanceAmount = balamount;
         }
         public override void Debit(double debitamount)
         {
+            if (debitamount > BalanceAmount)
+                throw new ArgumentException("Withdrawl amount cannot be greater than Account Balance");
+            if (debitamount > 1000.00)
+                throw new ArgumentException("Cannot transfer more than 1000.00");
             BalanceAmount = BalanceAmount - (debitamount + (debitamount * 0.025));
             balanceAmount("Withdrawn",debitamount);
+            var transaction = new Transaction()
+            {
+                TransactionDate = DateTime.Now,
+                TransactionType = TransactionType.Withdrawal,
+                TransactionAmount = debitamount,
+                TransactionHolderName = AccountHolderName[0],
+                TransactionAc = AccountNumber
+            };
+            InsertTransaction(transaction);
+
         }
     }
+
 }
--- FirstAssign/FirstAssign/Program.cs
+++ FirstAssign/FirstAssign/Program.cs
@@ -11,14 +11,18 @@ namespace FirstAssign
     {
         static void Main(string[] args)
         {
-            SavingsAccount savobj = new SavingsAccount(1234, new List<string>() { "Steve", "Hill" }, "16 - 06 - 2021", 10000.00);
-            CheckingAccount checkobj = new CheckingAccount(1234, new List<string>() { "Joseph" }, 0.00);
-            savobj.Debit(500);
-            savobj.Credit(1000);
-            checkobj.Credit(6000);
-            checkobj.Debit(1000);
-            savobj.Credit(5000);
-
+            SavingsAccount savobj = new SavingsAccount(123456789, new List<string>() { "Steve", "Hill" }, "16 - 06 - 2021", 10000.00);
+            savobj.Debit(500.00);
+            savobj.Credit(900.00);
+            savobj.Credit(1000.00);
+            savobj.Debit(1000.00);
+            savobj.Debit(800.00);
+            savobj.Credit(600.00);
+            savobj.ViewTransactions();
+            CheckingAccount checkobj = new CheckingAccount(167243568, new List<string>() { "Joseph" }, 0.00);
+            checkobj.Credit(1000.00);
+            checkobj.Debit(300.00);
+            checkobj.ViewTransactions();
             Console.ReadLine();
 
         }
